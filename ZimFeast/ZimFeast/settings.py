4c6a1aebd27277d33e54333a3513d19e93a61899
Brandon <Apex2.0predator@gmail.com>
2026-01-07 10:15:54 +0200

    added zimfeast app

--- ZimFeast/ZimFeast/settings.py
+++ ZimFeast/ZimFeast/settings.py
@@ -152,11 +152,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/'
-STATIC_ROOT = BASE_DIR.parent / 'dist' / 'public'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/'
+STATIC_ROOT = BASE_DIR.parent / 'dist' / 'public'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
